ad57c46705a5114daca61745a86c74ab1edbefe3
antoanstin <antoanstin@gmail.com>
2023-07-28 16:47:38 +0530

    add

---
+++ main.js
@@ -0,0 +1,24 @@
+$('.brand').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:4,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:true
+        }
+    }
+})
+var btn=document.querySelector(".burger-icon");
+btn.addEventListener("click",function(e){
+    e.target.parentNode.querySelector(".header-res-menu").classList.toggle("calloff")
+})
